32e17ef2304c9676d41c92d91385c77c3e4f80ad
Marius Van Hooreweghe <marius.vanhooreweghe.w3546@student.hogent.be>
2017-12-08 05:30:50 +0100

    added link to heroku website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,27 +7,6 @@ import { AuthenticationService } from './user/authentication.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  //public promises: Promise[];
-  //private _promiseDataService: PromiseDataService;
-  //constructor(private _dataService : PromiseDataService){
-  //  this._promiseDataService = this._dataService;
-  //}
-  //ngOnInit(){
-  //  this._promiseDataService.promises.subscribe(items => this.promises = items);
-  //}
-  //newPromiseAdded(promise: Promise){
-  //  this._promiseDataService.addNewPromise(promise).subscribe(item => {
-  //    const promisee = promise.promisees.map(u => 
-  //      this._promiseDataService.addPromiseeToPromise(u, item));
-  /*    Observable.forkJoin(...promisee).subscribe( 
-        (promisees: Promisee[]) => {
-          for (const u of promisees) {
-            item.addPromisee(u);
-          }
-          return this.promises.push(item)
-        }); 
-    });
-  }*/
   constructor(private authService: AuthenticationService) {
   }
 
